559d13a9c8498e8af6cffce23618900af1329893
Gary1q2 <gary1q2@hotmail.com>
2019-11-19 20:20:48 +1100

    What da

--- part3.py
+++ part3.py
@@ -12,13 +12,34 @@ from imdb_dataloader import IMDB
 class Network(tnn.Module):
     def __init__(self):
         super(Network, self).__init__()
+        self.conv1 = tnn.Sequential(
+            tnn.Conv1d(in_channels=50, out_channels=50, kernel_size=8, padding=5),
+            tnn.ReLU(),
+            tnn.MaxPool1d(kernel_size=4))
+        self.conv2 = tnn.Sequential(
+            tnn.Conv1d(in_channels=50, out_channels=50, kernel_size=8, padding=5),
+            tnn.ReLU(),
+            tnn.MaxPool1d(kernel_size=4))
+        self.conv3 = tnn.Sequential (
+            tnn.Conv1d(in_channels=50, out_channels=50, kernel_size=8, padding=5),
+            tnn.ReLU(),
+            tnn.AdaptiveMaxPool1d(output_size=1))
+        self.fc1 = tnn.Linear(in_features=50, out_features=1)
+
 
     def forward(self, input, length):
         """
         DO NOT MODIFY FUNCTION SIGNATURE
         Create the forward pass through the network.
         """
-
+        input = input.permute(0, 2, 1)
+        input = self.conv1(input)
+        input = self.conv2(input)
+        input = self.conv3(input)
+        input = input.view(input.shape[0], -1)
+        input = self.fc1(input)
+        input = torch.flatten(input)
+        return input
 
 class PreProcessing():
     def pre(x):
@@ -27,7 +48,7 @@ class PreProcessing():
 
     def post(batch, vocab):
         """Called after numericalization but prior to vectorization"""
-        return batch, vocab
+        return batch
 
     text_field = data.Field(lower=True, include_lengths=True, batch_first=True, preprocessing=pre, postprocessing=post)
 
@@ -37,6 +58,12 @@ def lossFunc():
     Define a loss function appropriate for the above networks that will
     add a sigmoid to the output and calculate the binary cross-entropy.
     """
+    def loss(output, labels):
+        output = torch.flatten(output)
+        lossFunc = torch.nn.BCEWithLogitsLoss()
+        return lossFunc(output, labels)
+
+    return loss
 
 def main():
     # Use a GPU if available, as it should be faster.
@@ -56,7 +83,7 @@ def main():
                                                          sort_key=lambda x: len(x.text), sort_within_batch=True)
 
     net = Network().to(device)
-    criterion =lossFunc()
+    criterion = lossFunc()
     optimiser = topti.Adam(net.parameters(), lr=0.001)  # Minimise the loss using the Adam algorithm.
 
     for epoch in range(10):
